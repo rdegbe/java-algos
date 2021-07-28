ca6638bde5afe39eb64e6ba6d6d4926c288c004d
Raymond Degbe <rdegbe14@gmail.com>
2021-07-27 20:20:33 -0400

    Improve Singly Linked List Implementation

--- src/com/raymond/algorithms/datastructures/linkedlist/SinglyLinkedList.java
+++ src/com/raymond/algorithms/datastructures/linkedlist/SinglyLinkedList.java
@@ -1,49 +1,127 @@
 /**
- * A very simple implementation of singly linked list.
+ * Implementation of singly linked list.
  *
  * @author Raymond Degbe, raymond.degbe.05@gmail.com
  */
 package com.raymond.algorithms.datastructures.linkedlist;
 
-public class SinglyLinkedList {
-    Node head; // head of list
+import java.util.NoSuchElementException;
 
-    // Linked list Node. This inner class is made static so that
-    // main() can access it
-    static class Node {
-        int data;
-        Node next;
+public class SinglyLinkedList{
+    private class Node{
+        private int data;
+        private Node next;
 
-        Node(int d) {
-            this.data = d;
-            next = null;
-        } // Constructor
-    }
-
-    // This function prints contents of linked list starting from head
-    public void printList() {
-        Node n = head;
-        while (n != null) {
-            System.out.print(n.data + " ");
-            n = n.next;
+        public Node(int data, Node next){
+            this.data = data;
+            this.next = next;
         }
     }
+    private int size = 0;
+    private Node head;
+    private Node tail;
+    private Node tbr;
+    private Node trav;
 
 
+    // Add function adds node at the tail of the linked list by default, O(1)
+    public void add(int elem) {
+        addLast(elem);
+        size++;
+    }
 
-// method to create a simple linked list with 3 nodes
+    // Add a node to the tail of the linked list, O(1)
+    public void addLast(int elem) {
+        if(isEmpty())
+            head = tail = new Node(elem, null);
+        else {
+            tail.next = new Node(elem, null);
+            tail = tail.next;
+        }
+        size++;
+    }
+
+    public void addFirst(int elem) {
+        if (isEmpty())
+            head = tail = new Node(elem, null);
+        else {
+            trav = new Node(elem, head);
+            head = trav;
+        }
+        size++;
+    }
+
+    public void removeFirst(){
+        if(isEmpty()) throw new NoSuchElementException();
+
+        if (head == tail ){
+            head = tail = null;
+            return;
+        }
+        tbr = head.next;
+        head = null;
+        head = tbr;
+        --size;
+    }
+
+    public void removeLast() {
+        if(isEmpty()) throw new NoSuchElementException();
+
+        if (head == tail ){
+            head = tail = null;
+            return;
+        }
+        trav = head;
+        while (hasNotEnd() ) {
+            if (trav.next == tail ) break;
+            trav = trav.next;
+        }
+        tail = trav;
+        tail.next = null;
+        --size;
+    }
+
+    public int indexOf(int elem){
+        int index = 0;
+        trav = head;
+        while (hasNotEnd()){
+            if (trav.data == elem) return index;
+            trav = trav.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public boolean contains(int elem){
+        return indexOf(elem) != -1;
+    }
+
+    // Return size of the linked list
+    public int size(){
+        return size;
+    }
+
+    public boolean hasNotEnd(){
+        return trav != null;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+
 public static void main(String[] args) {
-    /* Start with the empty list. */
-    SinglyLinkedList sllist = new SinglyLinkedList();
+        SinglyLinkedList a = new SinglyLinkedList();
+        a.addFirst(2);
+        a.addFirst(7);
+        a.addLast(6);
+        a.addLast(4);
 
-    sllist.head = new Node(1);
-    Node second = new Node(2);
-    Node third = new Node(3);
+        a.removeLast();
+        a.removeLast();
 
-    sllist.head.next = second; // Link first node with the second node
-    second.next = third; // Link second node with the third node
+    System.out.println(a.contains(5));
 
-    sllist.printList();
 }
 }
 
