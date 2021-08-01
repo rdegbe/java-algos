bfb451b5aaf1d28a0d815affc67b0d2730b5b662
Raymond Degbe <rdegbe14@gmail.com>
2021-08-01 09:33:41 -0400

    Add Hash table implementations

---
+++ src/main/java/com/raymond/algorithms/datastructures/hashtable/SecondaryHash.java
@@ -0,0 +1,12 @@
+/**
+ * Simple interface file for double hashing
+ *
+ * @author Raymond Degbe, raymond.degbe.05@gmail.com
+ */
+package main.java.com.raymond.algorithms.datastructures.hashtable;
+
+public interface SecondaryHash {
+
+  // A method for a second hash function
+  public int hashCode2();
+}
